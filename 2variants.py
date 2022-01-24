3b0bc6d037e2ec06249970a56b03097a299cd88b
Eletie <60183758+Eletie@users.noreply.github.com>
2022-01-24 18:24:32 +0000

    neko

---
+++ 2variants.py
@@ -0,0 +1,10 @@
+linoleja_cena = int(input("Ievadi linoleja cenu ")) #ievadīt linoleju cenu
+telpas_platums = int(input("Cik plata ir telpa? " )) #telpas platums
+telpas_garums = int(input("Cik gara ir telpa? ")) #telpas garums
+
+def gridas_izmaksas():
+  telpas_lielums = telpas_garums * telpas_platums #aprēķina telpas lielumu
+  linoleja_izmaksas = telpas_lielums * linoleja_cena #aprēķina linoleja izmaksas
+  print("Linoleja izmaksas ir ", str(linoleja_izmaksas), "eiro.") #izvada linoleja izmaksas
+
+gridas_izmaksas() #
