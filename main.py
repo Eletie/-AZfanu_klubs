73a3549529241db127c10d7eb1d7b903eda18928
Eletie <60183758+Eletie@users.noreply.github.com>
2022-01-24 19:16:43 +0000

    daudz ko mainiju heh

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-#Danils2
-print("kā iesākt")
+print("Linoleja ieklāšanas aprēķinu programma")
 
-linoleja_cena = int(input("Ievadi linoleja cenu ")) #ievadīt linoleju cenu
-telpas_platums = int(input("Cik plata ir telpa? " )) #telpas platums
-telpas_garums = int(input("Cik gara ir telpa? ")) #telpas garums
+linoleja_cena = int(input("Ievadi linoleja cenu EUR/m2 ")) #ievadīt linoleju cenu
+linoleja_platums = float(input("Ievadi linoleja platumu "))
+telpas_platums = float(input("Cik plata ir telpa? " )) #telpas platums
+telpas_garums = float(input("Cik gara ir telpa? ")) #telpas garums
 
-def gridas_izmaksas():
-  telpas_lielums = telpas_garums * telpas_platums #aprēķina telpas lielumu
-  linoleja_izmaksas = telpas_lielums * linoleja_cena #aprēķina linoleja izmaksas
-  print("Linoleja izmaksas ir ", str(linoleja_izmaksas), "eiro.")
+def gridas_izmaksa(linoleja_cena, linoleja_platums, telpas_platums, telpas_garums):
+  telpas_laukums = round(telpas_garums) * telpas_platums #aprēķina telpas lielumu
+  linojeja_izmaksas = int(telpas_laukums * linoleja_cena) #aprēķina linoleja izmaksas
+  return linojeja_izmaksas
 
-gridas_izmaksas() #ja cav
+
+print("grīdas izmaksas ir " + str(gridas_izmaksa(linoleja_cena, linoleja_platums, telpas_platums, telpas_garums)) + "eiro.")
