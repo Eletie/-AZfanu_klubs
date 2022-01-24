bad50f50699b560876e8785033d724554704b593
Eletie <60183758+Eletie@users.noreply.github.com>
2022-01-24 19:43:51 +0000

    pievienoju lai garums un platums noapaļojas uz augšu

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import math
+
 print("Linoleja ieklāšanas aprēķinu programma")
 
-linoleja_cena = int(input("Ievadi linoleja cenu EUR/m2 ")) #ievadīt linoleju cenu
+linoleja_cena = float(input("Ievadi linoleja cenu EUR/m2 ")) #ievadīt linoleju cenu
 linoleja_platums = float(input("Ievadi linoleja platumu "))
 telpas_platums = float(input("Cik plata ir telpa? " )) #telpas platums
 telpas_garums = float(input("Cik gara ir telpa? ")) #telpas garums
 
 def gridas_izmaksa(linoleja_cena, linoleja_platums, telpas_platums, telpas_garums):
-  telpas_laukums = round(telpas_garums) * telpas_platums #aprēķina telpas lielumu
-  linojeja_izmaksas = int(telpas_laukums * linoleja_cena) #aprēķina linoleja izmaksas
+  telpas_laukums = math.ceil(telpas_garums) * math.ceil(telpas_platums) #aprēķina telpas lielumu
+  linojeja_izmaksas = float(telpas_laukums * linoleja_cena) #aprēķina linoleja izmaksas
   return linojeja_izmaksas
 
 
